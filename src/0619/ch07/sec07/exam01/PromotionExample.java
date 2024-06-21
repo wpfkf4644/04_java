92878b482eddd7a8eccfebfff5e794b00eda8996
Angela <wpfkf4644@gmail.com>
2024-06-21 09:11:48 +0900

    add files

---
+++ src/0619/ch07/sec07/exam01/PromotionExample.java
@@ -0,0 +1,28 @@
+package ch07.sec07.exam01;
+
+class A {
+}
+class B extends A {
+}
+class C extends A {
+}
+class D extends B {
+}
+class E extends C {
+}
+public class PromotionExample {
+    public static void main(String[] args) {
+        B b = new B();
+        C c = new C();
+        D d = new D();
+        E e = new E();
+        A a1 = b;
+        A a2 = c;
+        A a3 = d;
+        A a4 = e;
+        B b1 = d;
+        C c1 = e;
+// B b3 = e;    상속 관계가 없으므로 컴파일 에러
+// C c2 = d;
+    }
+}
---
+++ src/0619/ch07/sec07/exam02/ChildExample.java
@@ -0,0 +1,12 @@
+package ch07.sec07.exam02;
+
+public class ChildExample {
+public static void main(String[] args) {
+    Child child = new Child();
+    Parent parent = new Parent();
+
+    parent.method1();
+    parent.method2();
+    child.method3();
+}
+}
